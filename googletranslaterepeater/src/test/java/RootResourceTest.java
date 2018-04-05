b47f87d43847a67586f137351cd3974c333c7e83
nathanbutler <n.butler001@gmail.com>
2018-04-04 21:34:14 -0600

    circleCI AWS config

--- googletranslaterepeater/src/test/java/RootResourceTest.java
+++ googletranslaterepeater/src/test/java/RootResourceTest.java
@@ -1,2 +1,14 @@
+import org.junit.Test;
+
+import static junit.framework.TestCase.assertEquals;
+
+/**
+ * This is a sample JUnit Test case
+ */
 public class RootResourceTest {
+        @Test
+    public static void addition_isCorrect() throws Exception {
+            assertEquals(4, 2+2);
+        }
+
 }
